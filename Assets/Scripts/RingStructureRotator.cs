399a41ea9c8b3735149da037846f72117527629f
sperezalonso <sergioperezalonsof@gmail.com>
2019-02-13 16:04:03 +0100

    Scene setup finished; added rotation script

--- Assets/Scripts/RingStructureRotator.cs
+++ Assets/Scripts/RingStructureRotator.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RingStructureRotator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class RingStructureRotator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject RoofStructure;
+    private Vector3 axis;        // axis by which the ring structure will rotate
+    private float speed;        // speed of rotation
+    
+	void Start ()
+	{
+	    axis = Vector3.up;
+	    speed = Time.deltaTime;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+	    // Rotate the ring structure in circles around the center of the scene
+	    transform.RotateAround(RoofStructure.transform.position, axis, speed);
 	}
 }
